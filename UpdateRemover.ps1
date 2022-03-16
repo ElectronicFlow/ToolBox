e9782a48c7e56e0b1b57359f8bdf26b805f71ddb
ElectronicFlow <101490264+ElectronicFlow@users.noreply.github.com>
2022-03-16 11:07:32 +0300

    Create UpdateRemover.ps1
     uninstall all Windows Update at once. They were uninstalled in order from the highest to the lowest KB Number which increases speed of the remove process

---
+++ UpdateRemover.ps1
@@ -0,0 +1,9 @@
+Get-WmiObject -query "Select HotFixID  from Win32_QuickFixengineering" | sort-object -Descending -Property HotFixID|%{
+    $sUpdate=$_.HotFixID.Replace("KB","")
+    write-host ("Uninstalling update "+$sUpdate);
+    & wusa.exe /uninstall /KB:$sUpdate /quiet /norestart;
+    # while((Get-Process "wusa" -ErrorAction SilentlyContinue).Count -ge 1){start-sleep 1}
+        # Waiting until an existing Hotfix installation finish. Improved by Paul T
+        Wait-Process wusa
+        Start-Sleep -s 1
+}
